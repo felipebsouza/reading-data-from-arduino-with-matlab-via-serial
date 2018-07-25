a0520eb01065acac98f73ef60091ffb752d0b4e1
Felipe Bittencourt <felipebs@gmail.com>
2018-07-25 17:59:55 -0300

    Fix typos

--- MATLAB_ARDUINO_SerialReadAndSaveToTXT.m
+++ MATLAB_ARDUINO_SerialReadAndSaveToTXT.m
@@ -1,24 +1,43 @@
 % Created by Felipe Bittencourt de Souza - August 2017.
-
-% This script was used to receive GPS data from an Arduino board in a computer using MATLAB, but it can be used to fetch data from any other serial connection.
-% The purpose was to plot the GPS positions in real time and save it in a log file using CVS format (data separated using a comma delimiter).
-
+%
+% Description:
+%
+%    This script was used to receive GPS data from an Arduino board in a computer using MATLAB, but it can be used to fetch data from any other serial connection.
+%    The purpose was to plot the GPS positions in real time and save it in a log file using CSV format (data separated using a comma delimiter).
+%
 % Requirements:
+%
 %   - Data must be sent through Serial port.
 %   - Variables must be sent separated by a comma ',' with a carriage return and newline characters after the last variable. Example: Serial.println()
-
-
+%
+% Usage:
+%
 % To use it with an Arduino:
+%
 %   - The Arduino board must be connected to the computer via USB port;
 %   - Data must be send using Serial.print() and Serial.println() functions.
-% Example:
-%       To send the following variables: Latitude = -401034; Longitude = -501343; RandomData = 12314;
-%               Serial.print("%d", Latitude)
-%               Serial.print("%d", Longitude)
-%               Serial.println("%d", RandomData)
+%
+%   Example:
+%       To send the following variables:
+%               Latitude = -401034;
+%               Longitude = -501343;
+%               RandomData = 12314;
+%
+%       Use something like:
+%               Serial.print(Latitude)
+%               Serial.print(",")
+%               Serial.print(Longitude)
+%               Serial.print(",")
+%               Serial.println(RandomData)
+%
+% Issues to improve:
+%     - Create a  decent' way to define number of variables and log header;
+%.    - Give a better attention to the UI control function;
+%     - If program freezes, sometimes the data is not saved properly.
+%
 
-%clear all
-%clc
+clear all
+clc
 format long
  
 % For macOS users: /dev/tty.usbmodem1411 
@@ -69,7 +88,3 @@ fclose(fileID);
 clear all
 clc
 
-% Things to improve:
-% - Create a decent way to define number of variables and log header.
-% - Give a better attention to the UI control function
-% - If program freezes, the data is not saved properly
